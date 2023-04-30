992670afed93359da37fccd1d400d728e7aba2f3
Martino Pagliarani <martipaglia02@gmail.com>
2023-04-30 16:26:04 +0200

    Updated

---
+++ METODI NUMERICI/Potenziamento/RisolviSis.py
@@ -0,0 +1,69 @@
+def Lsolve(L,b):
+    """  
+    Risoluzione con procedura forward di Lx=b con L triangolare inferiore  
+     Input: L matrice triangolare inferiore
+            b termine noto
+    Output: x: soluzione del sistema lineare
+            flag=  0, se sono soddisfatti i test di applicabilità
+                   1, se non sono soddisfatti
+    """
+#test dimensione
+    m,n=L.shape
+    flag=0;
+    if n != m:
+        print('errore: matrice non quadrata')
+        flag=1
+        x=[]
+        return x, flag
+    
+     # Test singolarita'
+    if np.all(np.diag(L)) != True:
+         print('el. diag. nullo - matrice triangolare inferiore')
+         x=[]
+         flag=1
+         return x, flag
+    # Preallocazione vettore soluzione
+    x=np.zeros((n,1))
+    
+    for i in range(n):
+         s=np.dot(L[i,:i],x[:i]) #scalare=vettore riga * vettore colonna
+         x[i]=(b[i]-s)/L[i,i]
+      
+     
+    return x,flag
+
+def Usolve(U,b):
+    
+    """
+    Risoluzione con procedura backward di Rx=b con R triangolare superiore  
+     Input: U matrice triangolare superiore
+            b termine noto
+    Output: x: soluzione del sistema lineare
+            flag=  0, se sono soddisfatti i test di applicabilità
+                   1, se non sono soddisfatti
+    
+    """ 
+#test dimensione
+    m,n=U.shape
+    flag=0;
+    if n != m:
+        print('errore: matrice non quadrata')
+        flag=1
+        x=[]
+        return x, flag
+    
+     # Test singolarita'
+    if np.all(np.diag(U)) != True:
+         print('el. diag. nullo - matrice triangolare superiore')
+         x=[]
+         flag=1
+         return x, flag
+    # Preallocazione vettore soluzione
+    x=np.zeros((n,1))
+    
+    for i in range(n-1,-1,-1):
+         s=np.dot(U[i,i+1:n],x[i+1:n]) #scalare=vettore riga * vettore colonna
+         x[i]=(b[i]-s)/U[i,i]
+      
+     
+    return x,flag

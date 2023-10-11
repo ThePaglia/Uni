923f06374ff99ad6d8075496c5075add5da12470
Martino Pagliarani <martino.pagliarani@studio.unibo.it>
2023-10-11 15:07:40 +0200

    CG formatted every es

--- CG/Lab/02 - Triangolo a colori/Triangolo a colori/ShaderMaker.h
+++ CG/Lab/02 - Triangolo a colori/Triangolo a colori/ShaderMaker.h
@@ -3,10 +3,10 @@
 
 class ShaderMaker
 {
-    public:
-		static GLuint createProgram(char* vertexfilename, char *fragmentfilename);
-		static char* readShaderSource(const char* shaderFile);
-	
-    private:
-		ShaderMaker() { }	
+public:
+	static GLuint createProgram(char *vertexfilename, char *fragmentfilename);
+	static char *readShaderSource(const char *shaderFile);
+
+private:
+	ShaderMaker() {}
 };
--- CG/Lab/07 - Farfalla/Farfalla/main.cpp
+++ CG/Lab/07 - Farfalla/Farfalla/main.cpp
@@ -10,21 +10,23 @@ static unsigned int programId;
 unsigned int VBO;
 unsigned int VAO;
 
-unsigned int nTriangles = 100;			// Pi� triangoli uso, pi� il cerchio sar� tondo
-unsigned int nVertices = nTriangles + 2;	// Deve collegare anche il centro e l'ultimo vertice
+unsigned int nTriangles = 100;			 // Pi� triangoli uso, pi� il cerchio sar� tondo
+unsigned int nVertices = nTriangles + 2; // Deve collegare anche il centro e l'ultimo vertice
 
 // Struct di un vertice (pixel)
-typedef struct {
+typedef struct
+{
 	float x, y, z, r, g, b, a;
 } Vertex;
 
-Vertex* circle = new Vertex[nVertices];
+Vertex *circle = new Vertex[nVertices];
 
 // Disegna un cerchio di colore nero al centro che sfuma verso il blu andando verso l'esterno
 // x(theta) = cx + raggiox * cos(theta)
 // y(theta) = cy + raggioy * sin(theta)
-void drawCircle(float cx, float cy, float rx, float ry, Vertex* circle) {
-	float step = 2 * PI / nTriangles;	// Angolo progressivo per disegnare i triangoli
+void drawCircle(float cx, float cy, float rx, float ry, Vertex *circle)
+{
+	float step = 2 * PI / nTriangles; // Angolo progressivo per disegnare i triangoli
 	circle[0].x = cx;
 	circle[0].y = cy;
 	circle[0].z = 0;
@@ -32,7 +34,8 @@ void drawCircle(float cx, float cy, float rx, float ry, Vertex* circle) {
 	circle[0].g = 1.0f;
 	circle[0].b = 0.0f;
 	circle[0].a = 1.0f;
-	for (int i = 0; i <= nVertices; i++) {
+	for (int i = 0; i <= nVertices; i++)
+	{
 		float theta_i = (float)i * step;
 		circle[i].x = cx + rx * ((sin(theta_i) * (exp(cos(theta_i)) - 2 * cos(4 * theta_i)) - pow(sin(theta_i / 12), 5)));
 		circle[i].y = cy + ry * ((cos(theta_i) * (exp(cos(theta_i)) - 2 * cos(4 * theta_i)) - pow(sin(theta_i / 12), 5)));
@@ -44,10 +47,11 @@ void drawCircle(float cx, float cy, float rx, float ry, Vertex* circle) {
 	}
 }
 
-void gestisci_shader(void) {
+void gestisci_shader(void)
+{
 	GLenum ErrorCheckValue = glGetError();
-	char* vertexShader = (char*)"vertexShaderSfumato.glsl";
-	char* fragmentShader = (char*)"fragmentShaderSfumato.glsl";
+	char *vertexShader = (char *)"vertexShaderSfumato.glsl";
+	char *fragmentShader = (char *)"fragmentShaderSfumato.glsl";
 	programId = ShaderMaker::createProgram(vertexShader, fragmentShader);
 	glUseProgram(programId);
 }
@@ -56,42 +60,42 @@ void gestisci_shader(void) {
 void INIT_VAO(void)
 {
 	drawCircle(0.0f, 0.0f, 0.1f, 0.1f, circle);
-	glGenVertexArrays(1, &VAO);	// Creo il VAO
+	glGenVertexArrays(1, &VAO); // Creo il VAO
 	glBindVertexArray(VAO);		// Faccio il bind (lo collego e lo attivo)
 
-	glGenBuffers(1, &VBO);		// Creo un VBO per le posizioni all'interno del VAO
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);	// Faccio il bind (lo collego e lo attivo assegnadogli il tipo GL_ARRAY_BUFFER)
-	glBufferData(GL_ARRAY_BUFFER, nVertices * sizeof(Vertex), &circle[0], GL_STATIC_DRAW);	// Carico i dati dei vertici sulla GPU
+	glGenBuffers(1, &VBO);																   // Creo un VBO per le posizioni all'interno del VAO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);													   // Faccio il bind (lo collego e lo attivo assegnadogli il tipo GL_ARRAY_BUFFER)
+	glBufferData(GL_ARRAY_BUFFER, nVertices * sizeof(Vertex), &circle[0], GL_STATIC_DRAW); // Carico i dati dei vertici sulla GPU
 
 	// Configurazione delle posizioni (stride = 3 -> x, y, z)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
 
 	// Configurazione dei colori (stride = 4 -> r, g, b, a)
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void *)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 }
 
 // Disegna i punti sulla finestra creata
 void drawScene(void)
 {
-	glBindVertexArray(0);				// Disattiva il VAO
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);	// Specifica il colore che la finestra deve assumere quando viene resettata
-	glClear(GL_COLOR_BUFFER_BIT);		// Pulisce il buffer del colore e setta il colore a quello definito prima
-	glBindVertexArray(VAO);				// Attiva il VAO
-	glDrawArrays(GL_TRIANGLE_FAN, 0, nVertices);	// Disegna li triangoli
-	glutSwapBuffers();					// Swap tra il front e back frame buffer durante l'animazione
+	glBindVertexArray(0);						 // Disattiva il VAO
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);		 // Specifica il colore che la finestra deve assumere quando viene resettata
+	glClear(GL_COLOR_BUFFER_BIT);				 // Pulisce il buffer del colore e setta il colore a quello definito prima
+	glBindVertexArray(VAO);						 // Attiva il VAO
+	glDrawArrays(GL_TRIANGLE_FAN, 0, nVertices); // Disegna li triangoli
+	glutSwapBuffers();							 // Swap tra il front e back frame buffer durante l'animazione
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	glutInit(&argc, argv);							// Inizializza il FreeGLUT toolkit
-	glutInitContextVersion(4, 0);					// Imposta la versione del contesto (OpenGL 4.0)
-	glutInitContextProfile(GLUT_CORE_PROFILE);		// Imposta il profilo del contesto
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);	// Specifica che verr� allocato un Buffer Display unico e i colori sono in formato RGBA
-	glutInitWindowSize(700, 700);					// Imposta la dimensione della finestra (pixel x pixel)
-	glutInitWindowPosition(400, 50);				// Imposta la distanza dall'angolo in alto a sinistra dello schermo
-	glutCreateWindow("Farfalla OpenGL");			// Crea una finestra sullo schermo e gli d� un titolo
+	glutInit(&argc, argv);						  // Inizializza il FreeGLUT toolkit
+	glutInitContextVersion(4, 0);				  // Imposta la versione del contesto (OpenGL 4.0)
+	glutInitContextProfile(GLUT_CORE_PROFILE);	  // Imposta il profilo del contesto
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // Specifica che verr� allocato un Buffer Display unico e i colori sono in formato RGBA
+	glutInitWindowSize(700, 700);				  // Imposta la dimensione della finestra (pixel x pixel)
+	glutInitWindowPosition(400, 50);			  // Imposta la distanza dall'angolo in alto a sinistra dello schermo
+	glutCreateWindow("Farfalla OpenGL");		  // Crea una finestra sullo schermo e gli d� un titolo
 	glutDisplayFunc(drawScene);
 	glewExperimental = GL_TRUE;
 	glewInit();

596c1dbc4617ee3de457afc3fe4afcbabc236133
Martino Pagliarani <martino.pagliarani@studio.unibo.it>
2023-12-10 11:22:22 +0100

    WEB Update CSS and template files

--- WEB/Lab/01-lab-html-materiale/template/contatti.php
+++ WEB/Lab/01-lab-html-materiale/template/contatti.php
@@ -1,27 +1,17 @@
 <section>
-    <h2>
-        Autori del Blog
-    </h2>
+    <h2>Autori del Blog</h2>
     <table>
-        <thead>
+        <tr>
+            <th id="autore">Autore</th>
+            <th id="email">Email</th>
+            <th id="argomenti">Argomenti</th>
+        </tr>
+        <?php foreach ($templateParams["autori"] as $autore) : ?>
             <tr>
-                <th id="autore">Autore</th>
-                <th id="email">Email</th>
-                <th id="argomenti">Argomenti</th>
+                <th id="<?php echo getIDFromName($autore["nome"]); ?>"><?php echo $autore["nome"]; ?></th>
+                <td headers="email <?php echo getIDFromName($autore["nome"]); ?>"><?php echo $autore["username"]; ?> </td>
+                <td headers="argormenti <?php echo getIDFromName($autore["nome"]); ?>"><?php echo $autore["argomenti"]; ?></td>
             </tr>
-        </thead>
-        <tbody>
-            <?php
-            foreach ($templateparams["autori"] as $autore) :
-            ?>
-                <tr>
-                    <th id="<?php echo getIDFromName($autore["nome"]); ?>"><?php echo $autore["nome"]; ?></th>
-                    <td headers="email <?php echo getIDFromName($autore["nome"]); ?>"><?php echo $autore["username"]; ?></td>
-                    <td headers="argomenti <?php echo getIDFromName($autore["nome"]); ?>"><?php echo $autore["argomentoo"] ?></td>
-                </tr>
-            <?php
-            endforeach;
-            ?>
-        </tbody>
+        <?php endforeach; ?>
     </table>
 </section>

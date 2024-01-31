574826fac2beff726a08c9948d82d19d29c4a7ff
Martino Pagliarani <martino.pagliarani@studio.unibo.it>
2024-01-31 12:18:41 +0100

    WEB json esame 20210610

--- WEB/Esami/20210610/compito-a/file/jquery/soluzione.js
+++ WEB/Esami/20210610/compito-a/file/jquery/soluzione.js
@@ -1,34 +1,68 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // dà le CORS policy e non so come farlo quindi vaffanculo
-    const button = document.querySelector("button");
-    button.addEventListener("click", function () {
-        fetch('data.json')
-            .then(response => response.json())
-            .then(data => {
-                const main = document.querySelector('main');
-                data.forEach(item => {
-                    const div = document.createElement('div');
-                    const ul = document.createElement('ul');
-                    const li1 = document.createElement('li');
-                    const li2 = document.createElement('li');
-                    const li3 = document.createElement('li');
-                    const upButton = document.createElement('button');
-                    const downButton = document.createElement('button');
-    
-                    li1.textContent = `id: ${item.id}`;
-                    li2.textContent = `nome: ${item.nome}`;
-                    li3.textContent = `type: ${item.type}`;
+document.addEventListener('DOMContentLoaded', function () {
+    // Ottieni il riferimento al bottone "Carica Dati"
+    var caricaDatiButton = document.querySelector('button');
+
+    // Aggiungi un gestore di eventi al clic sul bottone "Carica Dati"
+    caricaDatiButton.addEventListener('click', function () {
+        // Crea una nuova richiesta XMLHttpRequest
+        var xhr = new XMLHttpRequest();
+
+        // Configura la richiesta con il metodo GET e l'URL del file JSON
+        xhr.open('GET', 'data.json', true);
+
+        // Configura il gestore di eventi per la risposta alla richiesta
+        xhr.addEventListener("load", function () {
+            if (xhr.status == 200) {
+                // Analizza il JSON ottenuto dalla risposta
+                var jsonData = JSON.parse(xhr.responseText);
+
+                // Ottieni il riferimento al tag main
+                var mainElement = document.querySelector('main');
+
+                // Per ogni elemento nel vettore data, crea un div e aggiungi al tag main
+                jsonData.data.forEach(function (pokemon) {
+                    // Crea un nuovo div
+                    var newDiv = document.createElement('div');
+
+                    // Crea un elenco non ordinato
+                    var ulElement = document.createElement('ul');
+
+                    // Aggiungi ogni proprietà (id, nome e tipo) all'elenco non ordinato
+                    Object.keys(pokemon).forEach(function (key) {
+                        var liElement = document.createElement('li');
+                        liElement.textContent = key + ': ' + pokemon[key];
+                        ulElement.appendChild(liElement);
+                    });
+
+                    // Aggiungi l'elenco non ordinato al div
+                    newDiv.appendChild(ulElement);
+
+                    // Crea due bottoni (Up e Down)
+                    var upButton = document.createElement('button');
                     upButton.textContent = 'Up';
+                    upButton.addEventListener('click', function () {
+                        // Sposta il div sopra
+                        mainElement.insertBefore(newDiv, newDiv.previousElementSibling);
+                    });
+
+                    var downButton = document.createElement('button');
                     downButton.textContent = 'Down';
-    
-                    ul.appendChild(li1);
-                    ul.appendChild(li2);
-                    ul.appendChild(li3);
-                    div.appendChild(ul);
-                    div.appendChild(upButton);
-                    div.appendChild(downButton);
-                    main.appendChild(div);
+                    downButton.addEventListener('click', function () {
+                        // Sposta il div sotto
+                        mainElement.insertBefore(newDiv.nextElementSibling, newDiv);
+                    });
+
+                    // Aggiungi i bottoni al div
+                    newDiv.appendChild(upButton);
+                    newDiv.appendChild(downButton);
+
+                    // Aggiungi il nuovo div al tag main
+                    mainElement.appendChild(newDiv);
                 });
-            });
+            }
+        });
+
+        // Invia la richiesta
+        xhr.send();
     });
 });
